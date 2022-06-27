d28ad63ea2fb38e10d242515c8af21287fc3a381
bllw <bllw@bllws-MacBook-Pro.local>
2022-06-27 23:32:01 +0100

    update tables

--- src/pages/layouts/pagesAdmin/insciptionPage/InsciptionPage.js
+++ src/pages/layouts/pagesAdmin/insciptionPage/InsciptionPage.js
@@ -3,36 +3,21 @@ import React from "react";
 import { DataGrid } from '@mui/x-data-grid';
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-  },
+  { field: 'EmailAdress', headerName: 'Email Adress', width: 200 },
+  { field: 'firstName', headerName: 'First Name', width: 200 },
+  { field: 'lastName', headerName: 'Last Name', width: 200 },
+  { field: 'city', headerName: 'City', width: 200 },
+  { field: 'phoneNumber', headerName: 'Phone Number', width: 200 },
 ];
 
 const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  { id: 1, EmailAdress: 'Jon@gmail.com', firstName: 'lolo',lastName: 'snow', city: "agadir", phoneNumber: "0643526622" },
+  { id: 1, EmailAdress: 'Jon@gmail.com', firstName: 'lolo',lastName: 'snow', city: "agadir", phoneNumber: "0643526622" },
+  { id: 1, EmailAdress: 'Jon@gmail.com', firstName: 'lolo',lastName: 'snow', city: "agadir", phoneNumber: "0643526622" },
+  { id: 1, EmailAdress: 'Jon@gmail.com', firstName: 'lolo',lastName: 'snow', city: "agadir", phoneNumber: "0643526622" },
+  { id: 1, EmailAdress: 'Jon@gmail.com', firstName: 'lolo',lastName: 'snow', city: "agadir", phoneNumber: "0643526622" },
+  { id: 1, EmailAdress: 'Jon@gmail.com', firstName: 'lolo',lastName: 'snow', city: "agadir", phoneNumber: "0643526622" },
+
 ];
 
 export default function InsciptionPage() {
@@ -52,7 +37,6 @@ export default function InsciptionPage() {
           columns={columns}
           pageSize={5}
           rowsPerPageOptions={[5]}
-          
         />
       </div>
       {/* -------------end table-------------- */}
--- src/pages/layouts/pagesAdmin/messagesPage/MessagesPage.js
+++ src/pages/layouts/pagesAdmin/messagesPage/MessagesPage.js
@@ -1,4 +1,28 @@
 import React, { Component } from "react";
+import { DataGrid } from '@mui/x-data-grid';
+
+const columns = [
+  
+  { field: 'Email', headerName: 'Email', width: 250 },
+  { field: 'Message', headerName: 'Message', width: 600 },
+  {
+    field: 'Edit',
+    headerName: 'Edit',
+    width: 150,
+  },
+];
+
+const rows = [
+  { id: 1,Email: 'bllw@gmail.com', Message: 'Snow dkkdkeldeldmelmdlemdmeldmlemdmeld'},
+  { id: 2, Email: 'Lannister@', Message: 'Cersei' },
+  { id: 3, Email: 'Lannister@', Message: 'Jaime' },
+  { id: 4, Email: 'Stark@', Message: 'Arya'},
+  { id: 5, Email: 'Targaryen@', Message: 'Daenerys' },
+  { id: 6, Email: 'Melisandre@', Message: 'nulfrl' },
+  { id: 7, Email: 'Clifford@', Message: 'Ferrara'},
+  { id: 8, Email: 'Frances@', Message: 'Rossini' },
+  { id: 9, Email: 'Roxie@', Message: 'Harvey' },
+];
 
 class MessagesPage extends Component {
   render() {
@@ -10,6 +34,18 @@ class MessagesPage extends Component {
           <li className="breadcrumb-item active">Messages</li>
         </ol>
         {/* -------------end breadcrumb-------------- */}
+
+        {/* -------------start table-------------- */}
+        <div style={{ height: 400, width: '100%' }}>
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            pageSize={5}
+            rowsPerPageOptions={[5]}
+
+          />
+        </div>
+        {/* -------------end table-------------- */}
       </div>
     );
   }
